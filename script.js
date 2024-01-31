086cd1f165a72a37cd12a46e9cc08c6ae3f18b16
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-01-31 11:06:49 -0300

    accordion list, html, css ,javascript

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const btnMobile = document.querySelector("[data-menu='button']");
 const listMobile = document.querySelector("[data-menu='list']");
 const html = document.documentElement;
+const linksMenu = document.querySelectorAll("[data-menu='list'] li a");
+const accordion = document.querySelectorAll("[data-accordion='dt']");
 
-function handleClickOutside(event) {
-  if (!listMobile.contains(event.target) && !btnMobile.contains(event.target)) {
+function initMenuMobile() {
+  function handleClickOutside(event) {
+    if (!listMobile.contains(event.target) && !btnMobile.contains(event.target)) {
+      listMobile.classList.remove("active");
+      btnMobile.classList.remove("active");
+      html.removeEventListener("click", handleClickOutside);
+    }
+  }
+
+  function openMenu() {
+    listMobile.classList.toggle("active");
+    btnMobile.classList.toggle("active");
+    if (listMobile.classList.contains("active")) {
+      html.addEventListener("click", handleClickOutside);
+    } else {
+      html.removeEventListener("click", handleClickOutside);
+    }
+  }
+
+  function closeMenu() {
     listMobile.classList.remove("active");
     btnMobile.classList.remove("active");
     html.removeEventListener("click", handleClickOutside);
   }
+
+  btnMobile.addEventListener("click", openMenu);
+  linksMenu.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  })
 }
 
-function openMenu() {
-  listMobile.classList.toggle("active");
-  btnMobile.classList.toggle("active");
-  if (listMobile.classList.contains("active")) {
-    html.addEventListener("click", handleClickOutside);
-  } else {
-    html.removeEventListener("click", handleClickOutside);
+function initAccordion() {
+  accordion[0].classList.add("active");
+  accordion[0].nextElementSibling.classList.add("active");
+
+  function handleAccordion() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
   }
+
+  accordion.forEach((item) => {
+    item.addEventListener("click", handleAccordion);
+  })
 }
 
-btnMobile.addEventListener("click", openMenu);
+initMenuMobile();
+initAccordion();
