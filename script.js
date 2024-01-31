03bfc340a67ca99bd9d5bf06de408c0eb95187ba
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-01-30 23:00:22 -0300

    menu mobile javascript

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const btnMobile = document.querySelector("[data-menu='button']");
 const listMobile = document.querySelector("[data-menu='list']");
+const html = document.documentElement;
+
+function handleClickOutside(event) {
+  if (!listMobile.contains(event.target) && !btnMobile.contains(event.target)) {
+    listMobile.classList.remove("active");
+    btnMobile.classList.remove("active");
+    html.removeEventListener("click", handleClickOutside);
+  }
+}
 
 function openMenu() {
   listMobile.classList.toggle("active");
   btnMobile.classList.toggle("active");
+  if (listMobile.classList.contains("active")) {
+    html.addEventListener("click", handleClickOutside);
+  } else {
+    html.removeEventListener("click", handleClickOutside);
+  }
 }
 
 btnMobile.addEventListener("click", openMenu);
